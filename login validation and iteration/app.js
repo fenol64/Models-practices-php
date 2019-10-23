8d4e1053257621cad37ef69eccaca1b0a906b024
NOX-0202 <fernandinho.nco@gmail.com>
2019-10-23 14:00:03 -0300

    login system with js iteration

---
+++ login validation and iteration/app.js
@@ -0,0 +1,32 @@
+var containerElement, buttonElement, userElement, passElement;
+
+containerElement = document.querySelector('.container');
+userElement = document.getElementById('user_name');
+passElement = document.getElementById('user_pass');
+buttonElement = document.querySelector('#btn-send');
+
+function addClass (type){
+    containerElement.classList.add(type);
+    setTimeout(() => {
+        containerElement.classList.remove(type);
+    }, 1000);
+}
+
+buttonElement.onclick = () => {
+    let userInput, passInput;
+
+    userInput = userElement.value;
+    passInput = passElement.value;
+
+    if (userInput === "" || passInput === ""){
+        addClass("animationNo");
+    }else if (userInput === "admin" && passInput === "123") {
+        addClass("animationgo");
+    }else{
+        addClass("animationNo");
+    }
+
+    document.querySelector('form').reset();
+}
+
+
