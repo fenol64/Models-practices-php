f98378f20727c79b3017e56de0a628aeb7ab6766
nox-0202 <fernandinho.nco@gmail.com>
2020-04-21 20:34:01 -0300

    comments n index

--- oauth-login/index.php
+++ oauth-login/index.php
@@ -10,13 +10,20 @@ use CoffeeCode\Router\Router;
 $router = new Router(site());
 $router->namespace('Source\App');
 
+
+/*
+ * WEB
+ */
+
 $router->group(null);
 $router->get("/", "Web:login", "web.login");
 $router->get("/cadastrar", "Web:register", "web.register");
 $router->get("/recuperar", "Web:forget", "web.forget");
 $router->get("/senha/{email}/{forget_id}", "Web:reset", "web.reset");
 
-
+/*
+ * AUTH
+ */
 $router->group(null);
 $router->post("/login", "Auth:login", "auth.login");
 $router->post("/register", "Auth:register", "auth.register");
@@ -24,17 +31,21 @@ $router->post("/forget", "Auth:forget", "auth.forget");
 $router->post("/reset", "Auth:reset", "auth.reset");
 
 
-
+/*
+ * PROFILE
+ */
 $router->group("me");
 $router->get("/", "Dashboard:home", "dashboard.home");
 $router->get("/sair", "Dashboard:logoff", "dashboard.logoff");
 
 
-
+/*
+ * FOR ERRORS
+ */
 $router->group('ooops');
 $router->get("/{errcode}", "Web:error", "web.error");
 
-
+// executins routes
 $router->dispatch();
 
 if ($router->error()){
